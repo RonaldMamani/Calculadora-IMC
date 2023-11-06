44b71199d02e2252b9ea76126eac8afd77c5f4f3
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-11-06 20:07:26 -0300

    Adicionando toda a parte de input e lado esquerdo da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import styles from './App.module.css'
 import logo from './assets/powered.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weighttField, setWeightField] = useState<number>(0)
+
+  const handleCalculateIMC = () => {
+    if(heightField && weighttField) {
+
+    } else {
+      alert('Preencha todos os Campos')
+    }
+  }
 
   return (
     <div className={styles.main}>
@@ -14,7 +23,26 @@ function App() {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para indice de Massa Corpórea, paràmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa</p>
+          <input 
+            type="number" 
+            placeholder='Digite sua Altura Ex: 1.8 (Em Metros)'
+            className=''
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(+e.target.value)}
+          />
+          <input 
+            type="number" 
+            placeholder='Digite seu Peso Ex: 75 (Em Kilos)'
+            className=''
+            value={weighttField > 0 ? weighttField : ''}
+            onChange={e => setWeightField(+e.target.value)}
+          />
+          <button
+            className=''
+            onClick={handleCalculateIMC}
+          >Calcular IMC</button>
         </div>
         <div className={styles.rightSide}>
           ...
