a4e3e3db92ec389f833f58a27c214dba3a58fc00
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-11-07 21:06:41 -0300

    Adicionando estilização e renderizando lado direito com informações do Imc

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -1,4 +1,7 @@
 import { Level } from "../../helpers/imc"
+import styles from './GridItem.module.css'
+import upImage from '../../assets/up.png'
+import downImage from '../../assets/down.png'
 
 type Props = {
     item: Level
@@ -6,8 +9,20 @@ type Props = {
 
 export const GridItem = ({item} : Props) => {
     return (
-        <div>
-            {item.title}
+        <div className={styles.main} style={{backgroundColor: item.color}}>
+            <div className={styles.gridIcon}>
+                <img 
+                    src={item.icon === 'up' ? upImage : downImage} 
+                    alt={item.color === 'up' ? 'Icone Positivo' : 'Icone Negativo'} 
+                    width={30} 
+                />
+            </div>
+            <div className={styles.gridTitle}>
+                <h4>{item.title}</h4>
+            </div>
+            <div className={styles.gridInfo}>
+                <p>IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong></p>
+            </div>
         </div>
     )
 }
